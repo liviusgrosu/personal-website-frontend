c8c27e8dfbc2a8b3ef442591c017663cce910943
liviusgrosu <liviusgrosu@gmail.com>
2024-08-05 22:03:08 -0400

    Changed default vite build folder name

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/personal-website-frontend',
+  build: {
+    outDir: 'build'  // Change this to 'build' if you want the output in a 'build' folder
+  }
 })
