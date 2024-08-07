c66a948d45d5dcec9bdd51e0fc586e58c97e8b6f
liviusgrosu <liviusgrosu@gmail.com>
2024-08-06 22:01:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-website-frontend',
-  build: {
-    outDir: 'build'  // Change this to 'build' if you want the output in a 'build' folder
-  }
+  base: '/personal-website-frontend/'
 })
