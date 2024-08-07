7d56a0d202c37a5f68994bd5841525c5c01d74d3
liviusgrosu <liviusgrosu@gmail.com>
2024-08-06 22:01:42 -0400

    temp disable a few routes

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -1,41 +1,40 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { /*Navigate,*/ createBrowserRouter } from "react-router-dom";
 import App from "../../App";
 import AboutPage from "../../features/AboutPage"
 import ContactPage from "../../features/ContactPage";
 import ProjectsPage from "../../features/ProjectsPage";
-import ProjectDetail from "../../features/ProjectDetail";
+// import ProjectDetail from "../../features/ProjectDetail";
 import BlogPosts from "../../features/BlogPosts";
-import BlogPostDetail from "../../features/BlogPostDetail";
+// import BlogPostDetail from "../../features/BlogPostDetail";
 import LoginForm from "../../features/LoginForm";
-import ProjectEdit from "../../features/ProjectEdit";
-import BlogPostEdit from "../../features/BlogPostEdit";
-import AboutEdit from "../../features/AboutEdit";
-import RequireAuth from "./RequireAuth";
-import NotFound from "../../errors/NotFound";
+// import ProjectEdit from "../../features/ProjectEdit";
+// import BlogPostEdit from "../../features/BlogPostEdit";
+// import AboutEdit from "../../features/AboutEdit";
+// import RequireAuth from "./RequireAuth";
+// import NotFound from "../../errors/NotFound";
 export const router = createBrowserRouter([
     {
-        path: '/',
+        path: '/personal-website-frontend/',
         element: <App/>,
         children: 
         [
-            {element: <RequireAuth/>, children: [
-                { path: 'about-edit', element: <AboutEdit/> },
-                { path: 'projects-edit/:id', element: <ProjectEdit key='edit'/>},
-                { path: 'projects-create', element: <ProjectEdit key='create'/>},
-                { path: 'blog-edit/:id', element: <BlogPostEdit key='edit'/> },
-                { path: 'blog-create', element: <BlogPostEdit key='create'/> }
-            ]},
+            // {element: <RequireAuth/>, children: [
+            //     { path: 'about-edit', element: <AboutEdit/> },
+            //     { path: 'projects-edit/:id', element: <ProjectEdit key='edit'/>},
+            //     { path: 'projects-create', element: <ProjectEdit key='create'/>},
+            //     { path: 'blog-edit/:id', element: <BlogPostEdit key='edit'/> },
+            //     { path: 'blog-create', element: <BlogPostEdit key='create'/> }
+            // ]},
             { path: 'about', element: <AboutPage/> },
             { path: 'projects', element: <ProjectsPage/> },
-            { path: 'projects/:id', element: <ProjectDetail/> },
+            // { path: 'projects/:id', element: <ProjectDetail/> },
             { path: 'blog', element: <BlogPosts/> },
-            { path: 'blog/:id', element: <BlogPostDetail/> },
-            { path: 'contact', element: <ContactPage/> },
+            // { path: 'blog/:id', element: <BlogPostDetail/> },
             { path: 'login', element: <LoginForm/>},
             { path: 'contact', element: <ContactPage/>},
-            { path: '/', element: <Navigate to="/about" /> },
-            {path: 'not-found', element: <NotFound/>},
-            { path: '*', element: <Navigate replace to='/not-found'/>}
+            // { path: '/personal-website-frontend', element: <Navigate to="/personal-website-frontend/about" /> },
+            // { path: 'not-found', element: <NotFound/>},
+            // { path: '*', element: <Navigate replace to='/not-found'/>}
         ]
     }
 ])
